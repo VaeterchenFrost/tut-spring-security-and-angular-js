526b2c54139a31e3c1de1b636959f22f60dc5af0
Martin Röbke <martin.roebke@web.de>
2020-09-12 16:49:41 +0200

    Zuul Proxy

--- proxy/ui/src/main/java/demo/UiApplication.java
+++ proxy/ui/src/main/java/demo/UiApplication.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
-@EnableZuulProxy
+@EnableZuulProxy // Zuul Proxy
 @Controller
 public class UiApplication {
 
